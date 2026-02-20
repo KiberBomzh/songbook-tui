c4f0ea69d4c9f26e4f0e6549f9830ef0e975f932
KiberBomzh <klimenokandrej193@gmail.com>
2026-02-20 10:51:47 +0200

    added colors from hex in config

--- src/tui/config.rs
+++ src/tui/config.rs
@@ -4,6 +4,18 @@ use std::str::FromStr;
 use serde::{Serialize, Deserialize};
 use anyhow::Result;
 use ratatui::style::Color;
+use super::{
+    FOCUS_COLOR,
+    UNFOCUS_COLOR,
+    DIRECTORIES_COLOR,
+    SONGS_COLOR,
+
+    TITLE_COLOR,
+    CHORDS_COLOR,
+    RHYTHM_COLOR,
+    NOTES_COLOR,
+    TEXT_COLOR
+};
 
 
 
@@ -78,16 +90,16 @@ struct Song {
 impl Default for Colors {
     fn default() -> Self {
         Self {
-            focus: "light_green".to_string(),
-            unfocus: "dark_gray".to_string(),
-            directories: "blues".to_string(),
-            songs: "white".to_string(),
+            focus: FOCUS_COLOR.to_string(),
+            unfocus: UNFOCUS_COLOR.to_string(),
+            directories: DIRECTORIES_COLOR.to_string(),
+            songs: SONGS_COLOR.to_string(),
             song: Song {
-                title: "green".to_string(),
-                chords: "cyan".to_string(),
-                rhythm: "yellow".to_string(),
-                notes: "dark_gray".to_string(),
-                text: "white".to_string()
+                title: TITLE_COLOR.to_string(),
+                chords: CHORDS_COLOR.to_string(),
+                rhythm: RHYTHM_COLOR.to_string(),
+                notes: NOTES_COLOR.to_string(),
+                text: TEXT_COLOR.to_string(),
             }
         }
     }
@@ -150,6 +162,25 @@ impl Colors {
             {
                 Ok(Color::Rgb(*r, *g, *b))
             } else { Err(anyhow::anyhow!("Cannot get color!")) }
+        } else if color_str.starts_with("#") {
+            let hex = &color_str[1..];
+            let mut rgb: [u8; 3] = [255; 3];
+            let mut current_color: usize = 0;
+            let mut color_buf = String::new();
+            for c in hex.chars() {
+                if color_buf.len() < 2 {
+                    color_buf.push(c);
+                } else {
+                    if current_color < 3 {
+                        rgb[current_color] = u8::from_str_radix(&color_buf, 16)?;
+                        color_buf.clear();
+                        current_color += 1;
+                    } else { break }
+                }
+            }
+
+            let [r, g, b] = rgb;
+            return Ok(Color::Rgb(r, g, b))
         } else {
             Ok(Color::from_str(color_str)?)
         }
